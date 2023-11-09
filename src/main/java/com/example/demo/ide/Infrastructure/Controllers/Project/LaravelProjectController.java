1007dd510482ff0e206f17aa5a19a81e995eede3
Alexey Smirnov <smirnov@flagstudio.ru>
2023-11-10 00:24:21 +0500

    feat: begun styling a scene of laravel project creating

--- src/main/java/com/example/demo/ide/Infrastructure/Controllers/Project/LaravelProjectController.java
+++ src/main/java/com/example/demo/ide/Infrastructure/Controllers/Project/LaravelProjectController.java
@@ -3,18 +3,29 @@ package com.example.demo.ide.Infrastructure.Controllers.Project;
 import com.example.demo.ide.Infrastructure.Controllers.Controller;
 import com.example.demo.ide.UI.Scenes.Starting.Fork;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LaravelProjectController extends Controller {
     @FXML
-    public String name;
+    public TextField name;
 
     @FXML
-    public String path;
+    public TextField path;
+
+    @FXML
+    public Button createButton;
 
     @FXML
     public void comeBack() {
         this.stage.switchScene(this.context.getBean(Fork.class).load());
     }
+
+    public void create() {}
+
+    public void validate() {}
+
+    public void openFolderLocator() {}
 }
