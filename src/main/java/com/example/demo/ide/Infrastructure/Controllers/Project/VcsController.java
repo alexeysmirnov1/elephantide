e691a172d9a4ec545e7641b7178cddacd82b0511
Alexey Smirnov <smirnov@flagstudio.ru>
2023-11-15 23:07:40 +0500

    refactor: vcs creating

--- src/main/java/com/example/demo/ide/Infrastructure/Controllers/Project/VcsController.java
+++ src/main/java/com/example/demo/ide/Infrastructure/Controllers/Project/VcsController.java
@@ -1,44 +1,20 @@
 package com.example.demo.ide.Infrastructure.Controllers.Project;
 
-import com.example.demo.ide.Infrastructure.Controllers.Controller;
-import com.example.demo.ide.UI.Scenes.Starting.Fork;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.stage.DirectoryChooser;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-
 @Component
-public class VcsController extends Controller {
+public class VcsController extends AbstractProjectController {
     @FXML
     public TextField vcs;
-
-    @FXML
-    public TextField path;
-
-    @FXML
-    public Button createButton;
 
     public void initialize() {
         this.createButton.setDisable(true);
     }
 
-    public void comeBack() {
-        this.stage.switchScene(this.context.getBean(Fork.class).load());
-    }
+    public void create() {
 
-    public void create() {}
-
-    public void openFolderLocator() {
-        DirectoryChooser loader = new DirectoryChooser();
-        File directory = loader.showDialog(this.stage().getScene().getWindow());
-
-        if (directory != null) {
-            this.path.setText(directory.getPath());
-            this.validate();
-        }
     }
 
     public void validate() {
